3df8c0cdd84e4e8c36e7245247a3648595f45ec1
botshen <363088847@qq.com>
2021-06-01 22:14:22 +0800

    使用 ElementUI 库

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -4,5 +4,21 @@ export default {
     return {
       msg: "Welcome to Your Vue.js App"
     }
+  },
+  methods: {
+    click1() {
+      this.$message.error("错了哦，这是一条错误消息")
+    },
+    click2() {
+      this.$alert("这是一段内容", "标题名称", {
+        confirmButtonText: "确定",
+        callback: () => {
+          this.$message({
+            message: "恭喜你，这是一条成功消息",
+            type: "success"
+          })
+        }
+      })
+    }
   }
 }
--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>首页</p>
+  <div id="index"><p>首页</p>
+    <div>
+      <el-button @click="click1">默认按钮1</el-button>
+      <el-button @click="click2">默认按钮2</el-button>
+    </div>
+  </div>
+
 </template>
 <script src="./template.js"></script>
 <style scoped lang="less" src="./template.less"></style>
